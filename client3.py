c9c9e059c1b73aa90b06a51efbf9005361753a8b
ezz55 <ezzaldin55@gmail.com>
2024-03-15 00:01:10 +0000

    Update

--- client3.py
+++ client3.py
@@ -26,7 +26,7 @@ import urllib.request
 QUERY = "http://localhost:8080/query?id={}"
 
 # 500 server request
-N = 500
+N = 5000
 
 
 def getDataPoint(quote):
@@ -35,14 +35,16 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price+ask_price)/2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
+    if price_b == 0 :
+        return
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    return price_a/price_b
 
 
 # Main
@@ -53,7 +55,12 @@ if __name__ == "__main__":
 
         """ ----------- Update to get the ratio --------------- """
         for quote in quotes:
-            stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            if quote['stock'] == "ABC":
+                stock, bid_price, ask_price, price_a = getDataPoint(quote)
+                print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price_a))
+            else:
+                 stock, bid_price, ask_price, price_b = getDataPoint(quote)
+                 print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price_b))
 
-        print("Ratio %s" % getRatio(price, price))
+                       
+        print("Ratio %s" % getRatio(price_a, price_b))
